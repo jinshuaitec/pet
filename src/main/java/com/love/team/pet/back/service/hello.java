9fc0135abe1994c93d5de0c376e58645d881c2dc
jins <767957644@qq.com>
2018-08-04 13:34:21 +0800

    新建目录和mybatis生成器类, back是后台管理,wechat是微信的

---
+++ src/main/java/com/love/team/pet/back/service/hello.java
@@ -0,0 +1,8 @@
+package com.love.team.pet.back.service;
+
+/**
+ * @author jins
+ * @date on 2018/8/4.
+ */
+public class hello {
+}
---
+++ src/main/java/com/love/team/pet/wechat/service/hello.java
@@ -0,0 +1,8 @@
+package com.love.team.pet.wechat.service;
+
+/**
+ * @author jins
+ * @date on 2018/8/4.
+ */
+public class hello {
+}
