76604bd2b20d38f013049a073e1f99dce747e552
jins <767957644@qq.com>
2018-08-12 11:50:42 +0800

    提交fileUpload文件

---
+++ src/main/java/com/love/team/pet/model/FileUpload.java
@@ -0,0 +1,33 @@
+package com.love.team.pet.model;
+
+public class FileUpload extends BaseEntity {
+    private String name;
+
+    private String url;
+
+    private String ext;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+}
